191554dabacc8f6efb6f36445b2fdb4b336e880c
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2025-03-16 22:25:55 +0800

    Setting Up FIndWork Page

---
+++ src/FindWork.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './App.css';
+import logo from './assets/BlueHat_logo.png';
+import { Link } from "react-router-dom";
+
+const FindWork = () => {
+
+}
+
+export default FindWorkd
