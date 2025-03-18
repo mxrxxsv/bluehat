9aab08a9c8c5be48a7416336761e5289dc21ed44
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2025-03-18 22:04:24 +0800

    Update FindWork.jsx

--- src/FindWork.jsx
+++ src/FindWork.jsx
@@ -14,7 +14,7 @@ const FindWork = () => {
                         <img src={logo} className="h-10 w-28 md:h-15 md:w-40" alt="Flowbite Logo" />
                     </Link>
                         <div className="flex md:items-center md:space-x-4 md:order-2">
-                        <div className="w-42 h-10"></div>
+                        <div className="w-38 h-10"></div>
                         <button 
                             type="button" 
                             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
